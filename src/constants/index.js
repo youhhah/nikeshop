35bdbae25825dde5d28839f06e72f0870c6545bc
youhhah <77245644+youhhah@users.noreply.github.com>
2024-12-19 01:04:01 +0300

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -80,7 +80,7 @@ export const reviews = [
     },
     {
         imgURL: customer2,
-        customerName: 'Lota Mongeskar',
+        customerName: 'Dikaev Ahmed',
         rating: 4.5,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
     }
